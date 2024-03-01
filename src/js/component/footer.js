9073071fbaa2a36816b957e3f5470ebb08a241da
kjatkinson <149319433+kjatkinson@users.noreply.github.com>
2024-03-01 00:56:02 +0000

    navbar added footer need to b fixed

---
+++ src/js/component/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer(){
+    return (
+      <footer>
+        <p>© {new Date().getFullYear()}""</p>
+      </footer>
+    );
+  }
+  
+export default Footer
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,12 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import App from "./component/app.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(
+  <div>
+    <App/>
+  </div>,
+  document.querySelector("#app")
+);
